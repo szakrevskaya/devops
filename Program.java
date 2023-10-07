8d1c52e1ea6b36a87e0d1d95b125aff190684133
Snezhana Zakreuskaya <snezhana.zakreuskaya@oxagile.com>
2023-10-07 21:12:06 +0300

    Commit 2

--- Program.java
+++ Program.java
@@ -1,3 +1,5 @@
-public static void main(String[] args){
-    System.out.println("Hello world!");	
+public class Program {
+    public static void main(String[] args){
+       System.out.println("Hello world!");	
+    }
 }
